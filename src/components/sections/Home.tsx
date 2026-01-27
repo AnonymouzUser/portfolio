35e50e17ea2a4969c4c30c683f9b03d1b3c2c91c
Brayan <mrx.source.mrx@gmail.com>
2026-01-27 12:36:59 -0500

    feat: change home career description

--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -6,7 +6,7 @@ export function Home() {
           <h1 className="homeTitle">Home</h1>
           <h1>Brayan Ocampo Lopez</h1>
           <h5>(devaspepito)</h5>
-          <h4>Technologist in software analysis and development.</h4>
+          <h4>Technologist in web software analysis and development.</h4>
         </div>
       </section>
     </>
