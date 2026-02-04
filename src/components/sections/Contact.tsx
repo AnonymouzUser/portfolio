a2a4941ef11bc9e190fb64a6f41f3ff4712caaa4
Brayan <mrx.source.mrx@gmail.com>
2026-02-04 15:33:59 -0500

    feat: New contact section

---
+++ src/components/sections/Contact.tsx
@@ -0,0 +1,42 @@
+import { Instagram, LinkedIn, Gmail } from "../ui/Svgs";
+
+export const Contact = () => {
+  const mailLink = import.meta.env.VITE_MAIL_LINK;
+  const linkedinLink = import.meta.env.VITE_LIKEDIN_LINK;
+  const igLink = import.meta.env.VITE_IG_LINK;
+
+  return (
+    <>
+      <section className="contactSection" id="contact">
+        <div className="contactContainer">
+          <h1 className="contactTitle">Contact Me</h1>
+          <div className="contactInfo">
+            <p className="message">
+              I'm available for remote work. You can contact me via direct
+              message.
+            </p>
+            <div className="contactLinks">
+              <ul>
+                <li>
+                  <a href={mailLink}>
+                    <Gmail width={30} height={30} />
+                  </a>
+                </li>
+                <li>
+                  <a href={linkedinLink}>
+                    <LinkedIn width={30} height={30} />
+                  </a>
+                </li>
+                <li>
+                  <a href={igLink}>
+                    <Instagram width={30} height={30} />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
