b6a71e767beecc9f812cd635b9473ec0c9a53dbc
Brayan <mrx.source.mrx@gmail.com>
2026-02-13 11:24:56 -0500

    feat: New page for experience data

---
+++ src/components/pages/ExperiencePage.tsx
@@ -0,0 +1,32 @@
+export const ExperiencePage = () => {
+  return (
+    <>
+      <div className="experiencePage">
+        <img
+          src="/img/andesbpo_logo.png"
+          alt="CompanyName"
+          className="companyLogo"
+        />
+        <h1 className="experienceTitle">Experience</h1>
+        <p className="experiencePeriod">Period</p>
+        <p className="role">Role</p>
+        <ul>
+          <li className="responsabilitie">Responsabilitie</li>
+          <li className="responsabilitie">Responsabilitie</li>
+          <li className="responsabilitie">Responsabilitie</li>
+          <li className="responsabilitie">Responsabilitie</li>
+          <li className="responsabilitie">Responsabilitie</li>
+          <li className="responsabilitie">Responsabilitie</li>
+          <li className="responsabilitie">Responsabilitie</li>
+        </ul>
+        <div className="experienceTechnologies">
+          <p className="tecnologie">Tecnologie</p>
+          <p className="tecnologie">Tecnologie</p>
+          <p className="tecnologie">Tecnologie</p>
+          <p className="tecnologie">Tecnologie</p>
+          <p className="tecnologie">Tecnologie</p>
+        </div>
+      </div>
+    </>
+  );
+};
