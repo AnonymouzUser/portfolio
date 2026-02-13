30a3ace54adcf52d2a03fbb07cc8224189ed9cad
Brayan <mrx.source.mrx@gmail.com>
2026-02-13 18:38:34 -0500

    feat: add map for render info

--- src/components/pages/ExperiencePage.tsx
+++ src/components/pages/ExperiencePage.tsx
@@ -1,31 +1,65 @@
+import { experience } from "../../data/experience.data";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
+
 export const ExperiencePage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      const scrollContainer = document.getElementById("root");
+      if (scrollContainer) {
+        scrollContainer.scrollTop = 0;
+      }
+    }, 100);
+  }, []);
+
   return (
     <>
+      <button onClick={() => navigate("/")} className="backButton">
+        ← Back
+      </button>
       <div className="experiencePage">
-        <img
-          src="/img/andesbpo_logo.png"
-          alt="CompanyName"
-          className="companyLogo"
-        />
-        <h1 className="experienceTitle">Experience</h1>
-        <p className="experiencePeriod">Period</p>
-        <p className="role">Role</p>
-        <ul>
-          <li className="responsabilitie">Responsabilitie</li>
-          <li className="responsabilitie">Responsabilitie</li>
-          <li className="responsabilitie">Responsabilitie</li>
-          <li className="responsabilitie">Responsabilitie</li>
-          <li className="responsabilitie">Responsabilitie</li>
-          <li className="responsabilitie">Responsabilitie</li>
-          <li className="responsabilitie">Responsabilitie</li>
-        </ul>
-        <div className="experienceTechnologies">
-          <p className="tecnologie">Tecnologie</p>
-          <p className="tecnologie">Tecnologie</p>
-          <p className="tecnologie">Tecnologie</p>
-          <p className="tecnologie">Tecnologie</p>
-          <p className="tecnologie">Tecnologie</p>
-        </div>
+        {experience.map((exp, index) => (
+          <div key={index} className="experienceCard">
+            <h1 className="companyExTitle">{exp.company}</h1>
+            <h5 className="experiencePeriod">
+              <p className="periodTitle">Period</p>
+              {exp.period}
+            </h5>
+            <h5 className="role">
+              <p className="roleTitle">Role</p>
+              {exp.role}
+            </h5>
+
+            <h5 className="description">
+              <p className="descriptionTitle">Description</p>
+              {exp.longDescription}
+            </h5>
+            <ul className="responsabilities">
+              <p className="responsabilitiesTitle">Responsabilities</p>
+              {exp.responsabilities?.map((res, resIndex) => (
+                <li key={resIndex} className="responsabilitie">
+                  {res}
+                </li>
+              ))}
+            </ul>
+            <div className="experienceTechnologies">
+              <p className="techTitle">Technologies Used:</p>
+              <div className="usedTechnologies">
+                {exp.tech?.map((tech, techIndex) =>
+                  Array.isArray(tech) && tech.length === 2 ? (
+                    <span
+                      key={techIndex}
+                      className="techSvg"
+                      dangerouslySetInnerHTML={{ __html: tech[1] }}
+                    />
+                  ) : null,
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
