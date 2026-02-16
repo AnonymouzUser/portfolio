bb849882e20f90fce291859d7dc6b0883ecb4b9b
Brayan <mrx.source.mrx@gmail.com>
2026-02-16 10:10:29 -0500

    feat: add projectpage route and id by id for projects and experiences

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,12 +1,13 @@
 import { Routes as RouterRoutes, Route } from "react-router";
-import { ExperiencePage } from "./pages";
+import { ExperiencePage, ProjectPage } from "./pages";
 import { App } from "../App";
 
 export const Routes = () => {
   return (
     <RouterRoutes>
       <Route path="/" element={<App />} />
-      <Route path="/experiencePage" element={<ExperiencePage />} />
+      <Route path="/experiencePage/:id" element={<ExperiencePage />} />
+      <Route path="/projectPage/:id" element={<ProjectPage />} />
     </RouterRoutes>
   );
 };
