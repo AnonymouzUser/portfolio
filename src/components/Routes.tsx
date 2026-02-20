2479a75ed10c9ff0930f39495888fb8cb46c3e84
Brayan <mrx.source.mrx@gmail.com>
2026-02-19 20:11:09 -0500

    feat: change how to navigate in portfolio

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -3,11 +3,11 @@ import { ExperiencePage, ProjectPage } from "./pages";
 import { App } from "../App";
 
 export const Routes = () => {
-  return (
-    <RouterRoutes>
-      <Route path="/" element={<App />} />
-      <Route path="/experiencePage/:id" element={<ExperiencePage />} />
-      <Route path="/projectPage/:id" element={<ProjectPage />} />
-    </RouterRoutes>
-  );
+	return (
+		<RouterRoutes>
+			<Route path="/" element={<App />} />
+			<Route path="/experiencePage/:id" element={<ExperiencePage />} />
+			<Route path="/projectPage/:id" element={<ProjectPage />} />
+		</RouterRoutes>
+	);
 };
